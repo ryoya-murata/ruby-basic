9276e26b096907b7ba8509894d3a17f13b536967
ryoya.working@gmail.com <ryoya.working@gmail.com>
2022-09-01 08:30:38 +0900

    practice method

---
+++ method.rb
@@ -0,0 +1,7 @@
+# 3つの数の平均を求めるメソッド
+def ave(first, second,third)
+    puts a = (first + second + third) / 3
+end
+
+ave(3,4,5)
+ave(10,20,30)
---
+++ string.rb
@@ -0,0 +1,3 @@
+# Stringクラスの実行
+puts String.new("こんにちは");
+puts "文字列".class()
